3d89364d5bf5d9fed9bbfe84908b0ade23df61c7
Roman Gulevich <r.gulevich@syberry.com>
2023-05-27 13:53:29 +0300

    Delete all config from service module and inject module mapper in mapper classes throught the new...()

--- module-service/src/main/java/com/mjc/school/service/mapper/AuthorMapper.java
+++ module-service/src/main/java/com/mjc/school/service/mapper/AuthorMapper.java
@@ -3,18 +3,14 @@ package com.mjc.school.service.mapper;
 import com.mjc.school.repository.model.entity.Author;
 import com.mjc.school.service.dto.AuthorDto;
 import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import java.util.Objects;
 
+@Component
 public class AuthorMapper implements BaseMapper<AuthorDto, Author> {
 
-    private final ModelMapper mapper;
-
-    @Autowired
-    public AuthorMapper(ModelMapper mapper) {
-        this.mapper = mapper;
-    }
+    private final ModelMapper mapper = new ModelMapper();
 
     @Override
     public Author toEntity(AuthorDto authorDto) {
--- module-service/src/main/java/com/mjc/school/service/mapper/NewsMapper.java
+++ module-service/src/main/java/com/mjc/school/service/mapper/NewsMapper.java
@@ -3,18 +3,14 @@ package com.mjc.school.service.mapper;
 import com.mjc.school.repository.model.entity.News;
 import com.mjc.school.service.dto.NewsDto;
 import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import java.util.Objects;
 
+@Component
 public class NewsMapper implements BaseMapper<NewsDto, News> {
 
-    private final ModelMapper mapper;
-
-    @Autowired
-    public NewsMapper(ModelMapper mapper) {
-        this.mapper = mapper;
-    }
+    private final ModelMapper mapper = new ModelMapper();
 
     @Override
     public News toEntity(NewsDto newsResponseDto) {
